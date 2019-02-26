e1ae870d7a5967f2092c0268feac41ea396f5a9e
= <=>
2019-02-26 13:30:32 -0300

    add ropsten network to deploy

--- truffle-config.js
+++ truffle-config.js
@@ -1,7 +1,30 @@
+require('dotenv').config();
+const HDWalletProvider = require('truffle-hdwallet-provider');
 const path = require("path");
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
-  contracts_build_directory: path.join(__dirname, "client/src/contracts")
+  contracts_build_directory: path.join(__dirname, "client/src/contracts"),
+
+  networks: {
+
+    ropsten: {
+      provider: function() {
+        return new HDWalletProvider(
+          procces.env.MNEMONIC,
+          `https://ropsten.infura.io/v3/${process.env.infuraKey}`
+        );
+      },
+      gasPrice: 25000000000,
+      network_id: 3
+    },
+
+    solc: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  }
 };
